086bf7b2cc12b438adaea9f4b99ab091ad1712da
supremacy1 <andrewohejiedeogbu@yahoo.com>
2024-02-27 18:30:27 +0100

    sample

--- src/componet/NavigationBar.js
+++ src/componet/NavigationBar.js
@@ -1,6 +1,8 @@
 import React from 'react';
+import '../cssfiles/NavigationBar.css';
 //import './cssfiles/NavigationBar.css'; // Import CSS file for styling
-
+import logo1 from '../imge/logo1.jpg'
+import asian from '../imge/asian.jpg'
 import Slideshow from './slid';
 function NavigationBar() {
   return (
@@ -14,7 +16,9 @@ function NavigationBar() {
       </ul>
     </nav>
     {/* <Slideshow /> */}
-    <h1 class="color-changing-text">Urgent Sale.U!</h1>
+    {/* <img src={asian} className="App-logo" alt="logo" /> */}
+    <img src={logo1} alt='logo' className='logo' />
+    {/* <h1 class="color-changing-text">Urgent Sale.U!</h1> */}
     </>
   );
 }
